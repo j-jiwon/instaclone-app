9718681f706777f7c615575dee9cb9291f06c6da
JIWON <ziwon16@gmail.com>
2020-03-16 19:34:41 +0900

    Navigation : PhotoNavigation

---
+++ navigation/MainNavigation.js
@@ -0,0 +1,26 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigation from "./TabNavigation";
+import PhotoNavigation from "./PhotoNavigation";
+
+const MainNavigation = createStackNavigator();
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <MainNavigation.Navigator
+        initialRouteName="TabNavigation"
+        headerMode="none"
+        mode="modal"
+      >
+        <MainNavigation.Screen name="TabNavigation" component={TabNavigation} />
+        <MainNavigation.Screen
+          name="PhotoNavigation"
+          component={PhotoNavigation}
+        />
+      </MainNavigation.Navigator>
+    </NavigationContainer>
+  );
+};
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,7 +1,6 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
@@ -10,25 +9,23 @@ import Notification from "../screens/Notifications";
 
 const TabNavigation = createBottomTabNavigator();
 
-export default () => {
+export default ({ navigation }) => {
   return (
-    <NavigationContainer>
-      <TabNavigation.Navigator initialRouteName="AuthHome" headerMode="none">
-        <TabNavigation.Screen name="Home" component={Home} />
-        <TabNavigation.Screen name="Profile" component={Profile} />
-        <TabNavigation.Screen
-          name="Add"
-          component={View}
-          listeners={{
-            tabPress: e => {
-              e.preventDefault();
-              console.log("add");
-            }
-          }}
-        />
-        <TabNavigation.Screen name="Notification" component={Notification} />
-        <TabNavigation.Screen name="Search" component={Search} />
-      </TabNavigation.Navigator>
-    </NavigationContainer>
+    <TabNavigation.Navigator>
+      <TabNavigation.Screen name="Home" component={Home} />
+      <TabNavigation.Screen name="Profile" component={Profile} />
+      <TabNavigation.Screen
+        name="Add"
+        component={View}
+        listeners={{
+          tabPress: e => {
+            e.preventDefault();
+            navigation.navigate("PhotoNavigation");
+          }
+        }}
+      />
+      <TabNavigation.Screen name="Notification" component={Notification} />
+      <TabNavigation.Screen name="Search" component={Search} />
+    </TabNavigation.Navigator>
   );
 };
