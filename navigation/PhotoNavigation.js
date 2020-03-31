23ee7d1702d3eb7fce29d92163c6b09388b56a0b
JIWON <ziwon16@gmail.com>
2020-03-31 19:16:26 +0900

    Upload : Select Photo fin.

--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -24,7 +24,7 @@ export default () => {
   return (
     <StackTabs.Navigator screenOptions={{ headerStyle: { ...stackStyles } }}>
       <StackTabs.Screen
-        options={{ headerShown: false }}
+        options={{ title: "Choose Photo" }}
         name="PhotoTabs"
         component={PhotoTabs}
       />
